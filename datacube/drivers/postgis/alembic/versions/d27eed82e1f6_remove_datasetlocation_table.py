ab5b02e01cf79fddb4bb6f1f724eeb2b16d8fbb2
Ariana Barzinpour <ariana.barzinpour@ga.gov.au>
2024-11-20 04:33:39 +0000

    attempt adding data migration logic

--- datacube/drivers/postgis/alembic/versions/d27eed82e1f6_remove_datasetlocation_table.py
+++ datacube/drivers/postgis/alembic/versions/d27eed82e1f6_remove_datasetlocation_table.py
@@ -12,6 +12,7 @@ Create Date: 2024-11-13 02:39:55.671819
 from typing import Sequence, Union
 
 from alembic import op
+import sqlalchemy as sa
 from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, UniqueConstraint, Index
 from sqlalchemy.dialects import postgresql as postgres
 from sqlalchemy.sql import func
@@ -32,27 +33,49 @@ def upgrade() -> None:
     op.add_column("dataset",
                   Column("uri_body", String, comment="The body of the uri."),
                   schema="odc")
+    # select first active location from DatasetLocation and insert into Dataset
+    conn = op.get_bind()
+    conn.execute("""WITH active_locs AS (
+           SELECT dataset_ref, uri_scheme, uri_body,
+           ROW_NUMBER() OVER (PARTITION BY dataset_ref ORDER BY added) AS rownum
+           FROM odc.location
+           WHERE archived IS NULL
+       )
+       WITH first_uri AS (
+           SELECT dataset_ref, uri_scheme, uri_body FROM active_locs WHERE rownum = 1
+       )
+       UPDATE dataset SET dataset.uri_scheme=loc.uri_scheme, dataset.uri_body=loc.uri_body
+       FROM odc.dataset dataset INNER JOIN first_uri loc ON dataset.id = loc.dataset_ref""")
+
     op.drop_table("location", schema="odc", if_exists=True)
 
 
 def downgrade() -> None:
-    op.create_table("location",
-                    Column("id", Integer, primary_key=True, autoincrement=True),
-                    Column("dataset_ref", postgres.UUID(as_uuid=True), ForeignKey("odc.dataset.id"), nullable=False,
-                           comment="The product this dataset belongs to"),
-                    Column("uri_scheme", String, nullable=False, comment="The scheme of the uri."),
-                    Column("uri_body", String, nullable=False, comment="The body of the uri."),
-                    Column("added", DateTime(timezone=True), server_default=func.now(), nullable=False,
-                           comment="when added"),
-                    Column("added_by", Text, server_default=func.current_user(), nullable=False,
-                           comment="added by whom"),
-                    Column("archived", DateTime(timezone=True), default=None, nullable=True, index=True,
-                           comment="when archived, null for the active location"),
-                    UniqueConstraint('uri_scheme', 'uri_body', 'dataset_ref'),
-                    Index("ix_loc_ds_added", "dataset_ref", "added"),
-                    METADATA,
-                    schema="odc",
-                    comment="Where data for the dataset can be found (uri).",
+    loc = op.create_table(
+        "location",
+        Column("id", Integer, primary_key=True, autoincrement=True),
+        Column("dataset_ref", postgres.UUID(as_uuid=True), ForeignKey("odc.dataset.id"), nullable=False,
+               comment="The product this dataset belongs to"),
+        Column("uri_scheme", String, nullable=False, comment="The scheme of the uri."),
+        Column("uri_body", String, nullable=False, comment="The body of the uri."),
+        Column("added", DateTime(timezone=True), server_default=func.now(), nullable=False, comment="when added"),
+        Column("added_by", Text, server_default=func.current_user(), nullable=False, comment="added by whom"),
+        Column("archived", DateTime(timezone=True), default=None, nullable=True, index=True,
+               comment="when archived, null for the active location"),
+        UniqueConstraint('uri_scheme', 'uri_body', 'dataset_ref'),
+        Index("ix_loc_ds_added", "dataset_ref", "added"),
+        METADATA,
+        schema="odc",
+        comment="Where data for the dataset can be found (uri).",
+        if_not_exists=True,
     )
+
+    conn = op.get_bind()
+    res = conn.execute(
+        sa.text("SELECT id, uri_scheme, uri_body FROM odc.dataset WHERE uri_body IS NOT NULL")
+    ).fetchall()
+    values = [{"dataset_ref": r.id, "uri_scheme": r.uri_scheme, "uri_body": r.uri_body} for r in res]
+    op.bulk_insert(loc, values)
+
     op.drop_column("dataset", "uri_scheme", schema="odc")
     op.drop_column("dataset", "uri_body", schema="odc")
