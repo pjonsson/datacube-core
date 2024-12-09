132ec4d955c26569aa7153d3825557e5a02a491d
Paul Haesler <linkjuggler@gmail.com>
2024-12-09 12:54:41 +1100

    Mypy fixes.

--- datacube/api/core.py
+++ datacube/api/core.py
@@ -683,7 +683,7 @@ class Datacube:
         coords = numpy.asarray([coord for coord, _ in groups])
         data = numpy.empty(len(coords), dtype=object)
         for i, (_, dss) in enumerate(groups):
-            data[i] = list(dss)
+            data[i] = dss  # types: ignore[assignment]
 
         sources = xarray.DataArray(data,
                                    dims=[group_by.dimension],
